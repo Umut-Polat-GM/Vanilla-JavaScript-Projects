7d652ed29d2b96bdb604b07a70fba1ee4e372d6f
Umut-Polat-GM <umut.polat.272@gmail.com>
2022-12-24 23:55:36 +0300

    Dark-mode project finished | Vanilla.js

--- 19-dark-mode/app.js
+++ 19-dark-mode/app.js
@@ -1,1 +1,23 @@
-console.log('dark mode');
+const toggleBtn = document.querySelector(".btn"); // Açma/kapama düğmesini seç
+const articlesContainer = document.querySelector(".articles"); // Makalelerin bulunduğu konteyneri seç
+
+toggleBtn.addEventListener("click", () => { // Açma/kapama düğmesine tıklama olay dinleyicisi ekle
+    document.documentElement.classList.toggle("dark-theme"); // Döküman elemanının sınıf listesine "dark-theme" sınıfını ekle/çıkar
+});
+
+const articlesData = articles.map((article) => { // Makaleler dizisini gez
+        const { title, date, length, snippet } = article; // Makale öğelerinin değişkenlerine ata
+        const formatDate = moment(date).format("MMMM Do, YYYY"); // Tarihi "Ay gün, yıl" şeklinde biçimlendir
+        return `<article class="post">
+          <h2>${title}</h2>
+          <div class="post-info">
+            <span>${formatDate}</span>
+            <span>${length} min okuma</span>
+          </div>
+          <p>
+            ${snippet}
+          </p>
+        </article>`; // Makale için HTML döndür
+    }).join(""); // Tüm makalelerin HTML'ini birleştir
+
+articlesContainer.innerHTML = articlesData; // Makaleler konteynerinin içeriğini makalelerin HTML'ine eşitle
